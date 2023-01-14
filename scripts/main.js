04dbd7d3d378fd22704421d325fa8406c6a880d4
Martin <martin.rostejnsky@email.cz>
2023-01-14 20:04:02 +0100

    mobile menu function

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function showMenu() {
+    var items = document.querySelectorAll(".nav-item");
+    Array.prototype.forEach.call(items, function(el) {
+        el.classList.toggle("active");
+    })
+}
+
+window.onclick = function(event) {
+    var active = document.querySelectorAll(".active")
+    if (!event.target.matches('.menubtn')) {
+        Array.prototype.forEach.call(active, function(el) {
+            el.classList.remove("active")
+        });
+    }
+}

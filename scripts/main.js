7658548aaf5ec63381f1db5524a9d1be01364111
Martin <martin.rostejnsky@email.cz>
2023-01-14 22:54:44 +0100

    complete mobile navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
 function showMenu() {
-    var items = document.querySelectorAll(".nav-item");
-    Array.prototype.forEach.call(items, function(el) {
-        el.classList.toggle("active");
-    })
+    document.querySelector("nav menu").classList.toggle("active");
 }
 
 window.onclick = function(event) {
-    var active = document.querySelectorAll(".active")
     if (!event.target.matches('.menubtn')) {
-        Array.prototype.forEach.call(active, function(el) {
-            el.classList.remove("active");
-            console.log(event.target);
-        });
+        document.querySelector("nav menu").classList.remove("active");
     }
 }

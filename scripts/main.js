02b80a78dbb7b959bf6d157569d1b1b81425fd0a
Martin <martin.rostejnsky@email.cz>
2023-01-15 19:08:18 +0100

    desktop header

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-function showMenu() {
-    document.querySelector("nav menu").classList.toggle("active");
-}
-
 window.onclick = function(event) {
     if (!event.target.matches('.menubtn')) {
         document.querySelector("nav menu").classList.remove("active");
     }
+}
+
+function showMenu() {
+    document.querySelector("nav menu").classList.toggle("active");
 }

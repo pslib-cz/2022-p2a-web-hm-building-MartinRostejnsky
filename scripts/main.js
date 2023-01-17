211994dfa5f86edfc088f4917f3459cec6f42c3e
Martin <martin.rostejnsky@email.cz>
2023-01-17 18:00:33 +0100

    button simplification

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
 window.onclick = function(event) {
     if (!event.target.matches('.menubtn')) {
         document.querySelector("nav menu").classList.remove("active");
+    } else {
+        document.querySelector("nav menu").classList.toggle("active");
     }
-}
-
-function showMenu() {
-    document.querySelector("nav menu").classList.toggle("active");
 }

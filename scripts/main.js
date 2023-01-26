6f3a3ba0fbb9fd8d419c8823794498f0b55052d6
Martin Roštejnský <91248036+MartinRostejnsky@users.noreply.github.com>
2023-01-26 19:47:11 +0100

    quickfix16

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-window.onclick = function(event) {
-    if (!event.target.matches('.menubtn')) {
-        document.querySelector("nav menu").classList.remove("active");
-    } else {
+window.onclick = (event) => {
+    if (event.target.matches('.menubtn')) {
         document.querySelector("nav menu").classList.toggle("active");
+    } else {
+        document.querySelector("nav menu").classList.remove("active");
     }
 }
